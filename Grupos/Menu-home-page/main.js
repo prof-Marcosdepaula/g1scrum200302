46fc988734b1690c6fbdca0b6d0d93799fc5587a
joaoa1402 <pistoridasilva@gmail.com>
2023-10-05 19:59:14 -0300

    dia 05-10 menu da home pronto

--- Grupos/Menu-home-page/main.js
+++ Grupos/Menu-home-page/main.js
@@ -1,21 +1,33 @@
-$(document).ready(function() {
-    //responsive menu toggle
-    $("#menutoggle").click(function() {
-        $('.xs-menu').toggleClass('displaynone');
-
-        });
-    //add active class on menu
-    $('ul li').click(function(e) {
-        e.preventDefault();
-        $('li').removeClass('active');
-        $(this).addClass('active');
+document.addEventListener('DOMContentLoaded', function() {
+    // responsive menu toggle
+    const menuToggle = document.getElementById('menutoggle');
+    menuToggle.addEventListener('click', function() {
+        const xsMenu = document.querySelector('.xs-menu');
+        xsMenu.classList.toggle('displaynone');
     });
-//drop down menu	
-        $(".drop-down").hover(function() {
-            $('.mega-menu').addClass('display-on');
-        });
-        $(".drop-down").mouseleave(function() {
-            $('.mega-menu').removeClass('display-on');
-        });
 
+    // add active class on menu
+    const menuItems = document.querySelectorAll('ul li');
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            menuItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+
+    // drop-down menu
+    const dropDowns = document.querySelectorAll('.drop-down');
+    dropDowns.forEach(function(dropDown) {
+        dropDown.addEventListener('mouseenter', function() {
+            const megaMenu = this.querySelector('.mega-menu');
+            megaMenu.classList.add('display-on');
+        });
+        dropDown.addEventListener('mouseleave', function() {
+            const megaMenu = this.querySelector('.mega-menu');
+            megaMenu.classList.remove('display-on');
+        });
+    });
 });
